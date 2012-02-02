5549ab7df74bf23b68fda33687469908f9161f18
Donald Mcgaughey <dnmcgoy@Donalds-MacBook-Pro.local>
2012-02-01 22:08:23 -0500

    Initial Commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by Donald McGaughey on 2/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitTestTests/GitTestTests.h
@@ -0,0 +1,13 @@
+//
+//  GitTestTests.h
+//  GitTestTests
+//
+//  Created by Donald McGaughey on 2/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GitTestTests : SenTestCase
+
+@end
